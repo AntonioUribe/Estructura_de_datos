e1d305c54f0ad036a537490edc0535aeb1253ff1
Antonio De Jesus Uribe Salazar <68526850+AntonioUribe@users.noreply.github.com>
2021-03-25 13:09:55 -0600

    Update evidencia1.1.py

--- evidencia1.1.py
+++ evidencia1.1.py
@@ -1,5 +1,4 @@
 articulos={}
-#HolaSoyFer
 while True:
     print("\n\tMain menu")
     print("1-Registrar una venta")
@@ -7,51 +6,39 @@ while True:
     print("X-Salir ")
     opcion = input("Elige una opcion")
     if opcion =='1':
+        monto_total=0
         print("Registrar")
         contador= max(articulos,default=0)+1
-        descripcion = input(f"Escribe la descripcion del articulo {contador}")
-        cantidad = int(input("Escribe la cantidad a comprar del articulo"))
-        precio= float(input(f"Escribe el precio de piezas vendidas"))
-        articulos[contador]=[descripcion.upper(),cantidad,precio,cantidad*precio]
-        print(f"\nN° {contador} de Ventas\nDescripcion: {descripcion}\t {cantidad}X ${precio}\tMonto Total: {cantidad*precio}\n")
+        articulos[contador]=[]
+        while opcion !='0':
+            descripcion = input(f"Escribe la descripcion de la venta {contador}: ")
+            cantidad = int(input("Escribe la cantidad a comprar del articulo"))
+            precio= float(input(f"Escribe el precio de piezas vendidas"))
+            compra = (descripcion.upper(),cantidad,precio,cantidad*precio)
+            monto_total=monto_total+cantidad*precio
+            articulos[contador].append(compra)
+            opcion=input("Escribe si deseas continuar (1-Continuar registrando/0-Dejar de registar: ")
+        print(f"N° {contador}")
+        for i in range(len(articulos[contador])):
+            print(f"Descripcion: {articulos[contador][i][0]}\t {articulos[contador][i][1]}X ${articulos[contador][i][2]}\tMonto Total: {articulos[contador][i][3]}\n")
+        print("\nMonto total a pagar: ",monto_total)
         input("<<ENTER>>")
+        
     elif opcion=='2':
+        total=0
         print("\n\tConsulta tus ventas\n")
-        buscar=input("Introduce la descripcion a buscar sobre la venta")
-        lista = [i for i in articulos if buscar.upper()==articulos[i][0]]
-        if len(lista)!=0:
-            for registro in lista:
-                print(f"N° {registro} de Ventas\nDescripcion: {articulos[registro][0]}\t {articulos[registro][1]}X ${articulos[registro][2]}\tTotal: {articulos[registro][3]}")
+        buscar=int(input("Introduce la descripcion a buscar sobre la venta"))
+        if buscar in articulos:
+            for consulta in articulos[buscar]:
+                print(f"Descripcion: {consulta[0]}\t {consulta[1]}X ${consulta[2]}\tMonto Total: {consulta[3]}\n")
+                total=total+consulta[3]
+            print(f"Precio a pagar {total}")
         else:
-            print("\nNo se ha encontrado ninguna venta\n")
-            input("<<ENTER>>")
+            print("\n\tNo se ha encontrado dicho numero de venta")
+        input("<<ENTER>>")
     elif opcion =='X':
         print("\nSaliendo...\n")
         break
     else:
         print("\n\nError vuelve a intentarlo\n\n")
         input("<<ENTER>>")
-
-'''
-def registrar(articulos):
-    print("Registrar")
-    contador= max(articulos,default=0)+1
-    descripcion = input(f"Escribe la descripcion del articulo {contador}")
-    cantidad = int(input("Escribe la cantidad a comprar del articulo"))
-    precio= float(input(f"Escribe el precio de piezas vendidas"))
-    articulos[contador]=[descripcion,cantidad,precio,cantidad*precio]
-    print(f"\nN° {contador} de Ventas\nDescripcion: {descripcion}\t {cantidad}X ${precio}\tTotal: {cantidad*precio}\n")
-    input("<<ENTER>>")
-    return articulos
-
-def consultar(articulos):
-    print("\n\tConsulta tus ventas\n")
-    buscar=input("Introduce la descripcion a buscar sobre la venta")
-    lista = [i for i in articulos if buscar==articulos[i][0]]
-    if len(lista)!=0:
-        for registro in lista:
-            print(f"N° {registro} de Ventas\nDescripcion: {articulos[registro][0]}\t {articulos[registro][1]}X ${articulos[registro][2]}\tTotal: {articulos[registro][3]}")
-    else:
-        print("\nNo se ha encontrado ninguna venta\n")
-        input("<<ENTER>>")
-'''
